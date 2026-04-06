d5183deeb582c4e2bad7aafb32a99cbec3a1944f
René Descartes <kenddygabriel841@gmail.com>
2026-04-06 11:59:55 -0300

    Add files via upload

--- Sistema_Cantina_PW/src/App.jsx
+++ Sistema_Cantina_PW/src/App.jsx
@@ -3,6 +3,7 @@ import logo from './assets/logo.png'
 import imagens from './assets/imagens.png'
 import './App.css'
 import { Helmet } from 'react-helmet';
+import { isMobile, isDesktop } from 'react-device-detect';
 
 
 // const windowHeight = window.innerHeight;
@@ -15,35 +16,65 @@ function App() {
     setWidth(screen.width);
   })
 
-  return (
-    <>
-      <header style={{backgroundColor: '#110E41', height: windowWidth*0.105, minHeight: 75, justifyContent: 'center', gap: windowWidth*0.5, alignItems: 'center', display: 'flex', width: windowWidth, padding: windowWidth*0.02, boxSizing: 'border-box'}}>
-        <img src={logo} style={{width: windowWidth*0.14, minWidth: 100}}/>
-        <div style={{gap: windowWidth*0.08, display: 'flex'}}>
-          <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
-            <button style={{backgroundColor: "#6B0707", width: windowWidth*0.1, minWidth: '4rem',  height: windowWidth*0.04, minHeight: '1.5rem', color: 'white', fontSize: windowWidth*0.015, borderRadius: 7, boxShadow: 'none', outline: 'none', border: 'none'}}>Entrar</button>
-          </a>
+  if(isMobile){
+    return(
+      <>
+        <header style={{backgroundColor: '#110E41', height: '10%', display: 'flex', boxSizing: 'border-box', padding: '2%', justifyContent: 'center', alignItems: 'center', width: '100%', padding: '3%', gap: '35%', marginBottom: '10%'}}>
+          <img src={logo} style={{width: '20%'}}/>
+
+          <div style={{display: 'flex', gap: '15%', width: '100%', alignItems: 'center'}}>
+            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+              <button style={{backgroundColor: "#6B0707", color: 'white', boxShadow: 'none', outline: 'none', border: 'none', fontSize: '100%', borderRadius: 7, width: '140%', height: '190%' }}>Entrar</button>
+            </a>
+            
+            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+              <button style={{backgroundColor: "#6B0707", color: 'white', boxShadow: 'none', outline: 'none', border: 'none', fontSize: '100%', borderRadius: 7, width: '120%', height: '190%'}}>Cadastrar</button>
+            </a> 
+          </div>
+        </header>
+
+        <div style={{justifyContent: 'center', alignItems: 'center', padding: '10%', boxSizing: 'border-box', display: 'flex', flexDirection: 'column'}}>
+          <div>
+            <h1 style={{fontFamily: 'Dancing Script', color: '#6B0707', textShadow: '-1px -1px 0 #000', fontSize: '350%', margin: '0 0 0 0'}}>Bem Vindo</h1>
+            <p style={{fontSize: '120%', fontFamily: 'sans-serif', textAlign: 'justify'}}>Este site visa reduzir o desperdício de alimentos por parte dos alunos da Escola Etec Bento Quirino, afim de ter melhor utilização dos alimentos preparados </p>
+          </div>
+
+          <img src={imagens} style={{width: '100%', marginTop: '10%'}}/>
+        </div>
+      </>
+    )
+  }else{
+    return (
+      <>
+        <header style={{backgroundColor: '#110E41', height: windowWidth*0.105, minHeight: 75, justifyContent: 'center', gap: windowWidth*0.5, alignItems: 'center', display: 'flex', width: windowWidth, padding: windowWidth*0.02, boxSizing: 'border-box'}}>
+          <img src={logo} style={{width: windowWidth*0.14, minWidth: 100}}/>
+          <div style={{gap: windowWidth*0.08, display: 'flex'}}>
+            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+              <button style={{backgroundColor: "#6B0707", width: windowWidth*0.1, minWidth: '4rem',  height: windowWidth*0.04, minHeight: '1.5rem', color: 'white', fontSize: windowWidth*0.015, borderRadius: 7, boxShadow: 'none', outline: 'none', border: 'none'}}>Entrar</button>
+            </a>
+            
+            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+              <button style={{backgroundColor: "#6B0707", width: windowWidth*0.1, minWidth: '4rem', height: windowWidth*0.04, minHeight: '1.5rem', color: 'white', fontSize: windowWidth*0.015, borderRadius: 7, boxShadow: 'none', outline: 'none', border: 'none'}}>Cadastrar</button>
+            </a> 
+          </div>
+        </header>
+  
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: windowWidth*0.05, width: windowWidth, flexDirection: 'column', boxSizing: 'border-box'}}>
           
-          <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
-            <button style={{backgroundColor: "#6B0707", width: windowWidth*0.1, minWidth: '4rem', height: windowWidth*0.04, minHeight: '1.5rem', color: 'white', fontSize: windowWidth*0.015, borderRadius: 7, boxShadow: 'none', outline: 'none', border: 'none'}}>Cadastrar</button>
-          </a> 
+          <div style={{marginBottom: windowWidth*0.06}}>
+            <h1 style={{fontFamily: 'Dancing Script', fontSize: windowWidth*0.06, color: '#6B0707', textShadow: '-1px -1px 0 #000'}}> Bem Vindo</h1>
+            <p style={{fontFamily: 'sans-serif', fontSize: windowWidth*0.015}}>Este site visa reduzir o desperdício de alimentos por parte dos alunos da Escola Etec Bento Quirino, afim de ter melhor utilização dos alimentos preparados</p>
+          </div>
+  
+          <div> 
+            <img src={imagens} style={{width: windowWidth*0.5}}></img>
+          </div>
+  
         </div>
-      </header>
-
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: windowWidth*0.05, width: windowWidth, flexDirection: 'column', boxSizing: 'border-box'}}>
-        
-        <div style={{marginBottom: windowWidth*0.06}}>
-          <h1 style={{fontFamily: 'Dancing Script', fontSize: windowWidth*0.06, color: '#6B0707', textShadow: '-1px -1px 0 #000'}}> Bem Vindo</h1>
-          <p style={{fontFamily: 'sans-serif', fontSize: windowWidth*0.015}}>Este site visa reduzir o desperdício de alimentos por parte dos alunos da Escola Etec Bento Quirino, afim de ter melhor utilização dos alimentos preparados</p>
-        </div>
-
-        <div> 
-          <img src={imagens} style={{width: windowWidth*0.5}}></img>
-        </div>
-
-      </div>
-    </>
-  )
+      </>
+    )
+  }
+  
 }
 
 export default App
