d7c0a78913d5d850174b5fb1cfa9a2dd84dfd818
René Descartes <kenddygabriel841@gmail.com>
2026-04-13 12:03:30 -0300

    Add files via upload

---
+++ Sistema_Cantina_PW/src/telas/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Loginscreen() {
+    return(
+        <>
+            <h1>Teste</h1>
+            <p>do caralho, viu?</p>
+        </>
+    )
+}
+
+export default Loginscreen;
